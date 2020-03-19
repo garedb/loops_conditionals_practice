b4259d6156239e829f0ced8ddd7774ee67469a8f
garedb <garedbarney@gmail.com>
2020-03-19 15:04:00 -0700

    loop practice

--- solutions.js
+++ solutions.js
@@ -1,2 +1,108 @@
-// YOUR NAME HERE
+//Gared Barney
+
+// console.log("Making your way in the world today takes everything you've got.");
+// console.log("Taking a break from all your worries, sure would help a lot.");
+// console.log("Wouldn't you like to get away?");
+// console.log("Sometimes you want to go");
+// console.log("Where everybody knows your name,");
+// console.log("and they're always glad you came.");
+// console.log("You wanna be where you can see,");
+// console.log("our troubles are all the same");
+// console.log("You wanna be where");
+// console.log("everybody knows");
+// console.log("Your name.");
+
+// SyntaxError: missing ) after argument list
+// SyntaxError: missing ) after argument list
+//TypeError: console.lo is not a function
+//TypeError: console.log(...) is not a function
+
+
+
+// 999 > 999 - false
+// 999 == 999 - true
+// 999 != 999 - false
+// -5 >= -4 - false
+// 100 <= -100 - false
+// 20 + 5 < 5 - false
+//81 / 9 == 9 - true
+//9 != 8 + 1 - false
+//"abc" == "abc" - false
+// "a" == "b" - false
+
+
+
+// 5 == 5 - true
+// 5 === 5 - true
+// 5 == "5" - false
+// 5 === "5" - false
+// 5 - true
+// 0 - false
+// "a" - true
+// "" - false
+// [] - false
+// !!true - false
+
+
+
+// 1. = is an Assignment Operator it is used to assign the value of variable 
+// or expression, while == is an Equal to Operator and it is a relation 
+// operator used for comparison.
+// 2. == compares two variables irrespective of data type while 
+// == compares two variables in a strict check, which means it 
+// checks for data type also then it returns true or false.
+
+
+
+// let i = 1;
+// while(i <= 1000) {
+// 	console.log("Ginger chocolate honey patties")
+// 	i += 1
+// }
+
+
+
+// let i = 1;
+// while(i <= 1000) {
+// 	console.log(i)
+// 	i += 1
+// }
+
+
+
+// let i = 1;
+// while(i <= 1000) {
+// 	console.log(`Current loop number is:`, i)
+// 	i += 1
+// }
+
+
+
+// for(let i = 0; i <= 100; i++) {
+// 	console.log(i)
+// }
+
+
+// for(let i = 7; i <= 635; i++) {
+// 	console.log(i)
+// }
+
+
+// let start = 0
+// let LIMIT = 100
+
+// for(i = start; i <= LIMIT; i++) {
+// 	console.log(i)
+// }
+
+
+
+
+
+
+
+
+
+
+
 
